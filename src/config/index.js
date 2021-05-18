55f52525f7442838d9f059b43b8cf13806b396cf
João <joao@ecobonuz.com>
2021-05-17 23:14:02 -0300

    att

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,11 @@
 export default {
   project: process.env.PROJECT,
+  database: {
+    URI: process.env.DATABASE_URI
+  },
+  server: {
+    secret: process.env.SERVER_SECRET
+  },
   aws: {
     id: process.env.AWS_ID,
     region: process.env.AWS_REGION
